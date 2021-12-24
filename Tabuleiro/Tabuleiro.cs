b42ce7127888840262ba0fccbddd5d87fe4e61f2
Dschinemann <diego.alexandre@hotmail.com>
2021-12-24 17:14:14 -0300

    tratando erros com exceçao personalizada

--- Tabuleiro/Tabuleiro.cs
+++ Tabuleiro/Tabuleiro.cs
@@ -14,14 +14,44 @@ namespace Xadres___Console.Tabuleiro
             pecas = new Peca[linha, coluna];
         }
 
-        public Peca Getpeca(int linha, int colula) {
-            return pecas[linha, colula];
+        public Peca Getpeca(int linha, int coluna) {
+            return pecas[linha, coluna];
         }
 
+        public Peca GetPeca(Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return GetPeca(pos) != null;
+        }
         public void ColocarPeca(Peca peca, Posicao posicao)
         {
+            if (ExistePeca(posicao))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição");
+            } 
             pecas[posicao.Linha, posicao.Coluna] = peca;
             peca.Posicao = posicao;
+        }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if(pos.Linha < 0 || pos.Linha>=Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição invalida");
+            }
         }
     }
 }
